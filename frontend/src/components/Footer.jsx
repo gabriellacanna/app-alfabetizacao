da5740093c1e66dbadc30d36b7a30603e66b89a2
root <root@Lacanna>
2024-10-23 21:16:44 -0300

    Fix: frontend tag aberta

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 // src/components/Footer.jsx
-function Footer() {
+const Footer = () => {
   return (
     <footer className="bg-white shadow-up py-4 mt-auto">
       <div className="container mx-auto px-4 text-center text-gray-600">
@@ -10,13 +10,13 @@ function Footer() {
             target="_blank"
             rel="noopener noreferrer"
             className="text-purple-600 hover:text-purple-800"
-          >
+          <a>
             Gabriel Lacanna
           </a>
         </p>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
